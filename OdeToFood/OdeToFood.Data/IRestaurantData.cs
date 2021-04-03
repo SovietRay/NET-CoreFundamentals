9401430128f635563acc447d87c4349b7cde6fd2
Mikhail Klishevich <x-ray@mail.ru>
2021-04-04 04:03:37 +0700

    Ch2Ex11
    Razor page with Restaurants
    Add Data and Core projects
    Singleton for Data
    Table with foreach Restaurants on razor Page

---
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,34 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurants();
+        public class InMemoryRestaurantData : IRestaurantData
+        {
+            List<Restaurant> restaurants;
+            public InMemoryRestaurantData()
+            {
+                restaurants = new List<Restaurant>()
+                {
+                    new Restaurant {Id = 1, Name = "Papa Pizza", Location = "Moscow", Cuisine = CuisineType.Italian},
+                    new Restaurant {Id = 2, Name = "Maharadja!", Location = "Balashiha", Cuisine = CuisineType.Indian},
+                    new Restaurant {Id = 3, Name = "Arriba", Location = "Moscow", Cuisine = CuisineType.Mexican},
+                    new Restaurant {Id = 4, Name = "Stop chili", Location = "Butovo", Cuisine = CuisineType.Mexican}
+                };
+            }
+            public IEnumerable<Restaurant> GetRestaurants()
+            {
+                return from r in restaurants
+                       orderby r.Name
+                       select r;
+            }
+        }
+    }
+}
