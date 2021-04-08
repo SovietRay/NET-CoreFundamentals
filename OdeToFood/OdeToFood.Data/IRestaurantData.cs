951cf92e0fa452a9b58ce5bd3c3d0716d11cb6ec
Mikhail Klishevich <x-ray@mail.ru>
2021-04-09 03:13:18 +0700

    Ch6Ex1
    Connected to db

--- OdeToFood/OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -1,7 +1,5 @@
 ﻿using OdeToFood.Core;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,59 +12,7 @@ namespace OdeToFood.Data
         Restaurant GetById(int id);
         Restaurant Update(Restaurant updatedRestaurent);
         Restaurant Add(Restaurant newRestaurant);
+        Restaurant Delete(int id);
         int Commit();
-    }
-    public class InMemoryRestaurantData : IRestaurantData
-    {
-        List<Restaurant> restaurants;
-        public InMemoryRestaurantData()
-        {
-            restaurants = new List<Restaurant>()
-                {
-                    new Restaurant {Id = 1, Name = "Papa Pizza", Location = "Moscow", Cuisine = CuisineType.Italian},
-                    new Restaurant {Id = 2, Name = "Maharadja!", Location = "Balashiha", Cuisine = CuisineType.Indian},
-                    new Restaurant {Id = 3, Name = "Arriba", Location = "Moscow", Cuisine = CuisineType.Mexican},
-                    new Restaurant {Id = 4, Name = "Stop chili", Location = "Butovo", Cuisine = CuisineType.Mexican},
-                    new Restaurant {Id = 5, Name = "Gugugug", Location = "Balashiha", Cuisine = CuisineType.None}
-                };
-        }
-        public IEnumerable<Restaurant> GetRestaurants()
-        {
-            return from r in restaurants
-                   orderby r.Name
-                   select r;
-        }
-        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
-        {
-            return from r in restaurants
-                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
-                   orderby r.Name
-                   select r;
-        }
-        public Restaurant GetById(int id)
-        {
-            return restaurants.SingleOrDefault(r => r.Id == id);
-        }
-        public Restaurant Update(Restaurant updatedRestaurent)
-        {
-            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurent.Id);
-            if (restaurant != null)
-            {
-                restaurant.Name = updatedRestaurent.Name;
-                restaurant.Location = updatedRestaurent.Location;
-                restaurant.Cuisine = updatedRestaurent.Cuisine;
-            }
-            return restaurant;
-        }
-        public Restaurant Add(Restaurant newRestaurant)
-        {
-            restaurants.Add(newRestaurant);
-            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
-            return newRestaurant;
-        }
-        public int Commit()
-        {
-            return 0;
-        }
     }
 }
---
+++ OdeToFood/OdeToFood.Data/InMemoryRestaurantData.cs
@@ -0,0 +1,71 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+                {
+                    new Restaurant {Id = 1, Name = "Papa Pizza", Location = "Moscow", Cuisine = CuisineType.Italian},
+                    new Restaurant {Id = 2, Name = "Maharadja!", Location = "Balashiha", Cuisine = CuisineType.Indian},
+                    new Restaurant {Id = 3, Name = "Arriba", Location = "Moscow", Cuisine = CuisineType.Mexican},
+                    new Restaurant {Id = 4, Name = "Stop chili", Location = "Butovo", Cuisine = CuisineType.Mexican},
+                    new Restaurant {Id = 5, Name = "Gugugug", Location = "Balashiha", Cuisine = CuisineType.None}
+                };
+        }
+        public IEnumerable<Restaurant> GetRestaurants()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+        public Restaurant Update(Restaurant updatedRestaurent)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurent.Id);
+            if (restaurant != null)
+            {
+                restaurant.Name = updatedRestaurent.Name;
+                restaurant.Location = updatedRestaurent.Location;
+                restaurant.Cuisine = updatedRestaurent.Cuisine;
+            }
+            return restaurant;
+        }
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            restaurants.Add(newRestaurant);
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            return newRestaurant;
+        }
+        public int Commit()
+        {
+            return 0;
+        }
+
+        public Restaurant Delete(int id)
+        {
+            var restaurant = restaurants.FirstOrDefault(r => r.Id == id);
+            if (restaurant != null)
+            {
+                restaurants.Remove(restaurant);
+            }
+            return restaurant;
+        }
+    }
+}
